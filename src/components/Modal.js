a1771286f8ed13d49b868f7cec34a8ea42152c73
Josh Hobson <joshuahobson1995@gmail.com>
2023-12-06 18:24:24 -0700

    css and additions

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,32 +11,45 @@ function Modal() {
             <div className='modal_left'> 
 
             <h2>About me.</h2>
-                <p>Hello, my name is Josh Hobson and I am a Full Stack Web Developer. Whether you are looking for a new member for your team, or just the freelancer who can get it all done, I am passionate to help you reach your Web Development needs. From design to database, let me be the answer to “How can we get this done?”</p>
+            <div className='intro_container'>
+                <p className='intro'>Hello, my name is <span className='jh_red'>Josh Hobson </span>and I am a <span className='jh_blue'>Full Stack Web Developer. </span> Whether you are looking for a new member for your team, or just the freelancer who can get it all done, I am passionate to help you reach your Web Development needs. From <span className='jh_yellow'>design to database </span>, let me be the answer to “How can we get this done?”</p>
+            </div>
 
             </div>
             <div className='modal_right'>
+                <div className='modal_right_header'>
                 <h2>Send me an Email.</h2>
+                <p>Lets get something working</p>
+                </div>
 
                <form className='contact_form'>
+        <div className='image_container'>
                 <img src={logo} alt='' className='form_logo'/>
+        </div>
+                <h5>Name</h5>
                <input
                     type='text'
-                    name='from_name'
-                    placeholder='Your name'
+                    name='from_name'     
                     className='name_input'
                 />
+                <h5>Email</h5>
                <input
                     type='text'
-                    name='from_email'
-                    placeholder='Email'
-                    className='name_input'
+                    name='from_email'   
+                    className='email_input'
                 />
-               <input
+                <h5>Message</h5>
+               <textarea
                     type='textarea'
-                    name='message'
-                    placeholder='Message'
-                    className='name_input'
-                />
+                    name='message'        
+                    className='message_input'
+                />  
+
+
+
+<div className='send_container'>
+                <button className='send'>send</button>
+</div>
                </form>
     
 
